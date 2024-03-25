69f819fb517dc99e7bc953a149c99d84ba0f0473
jose vitor bullentini sanches <jbullentinisanches@gmail.com>
2024-03-25 09:04:23 -0300

    aula segunda 25 03

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ def ao_conectar(client,userdata,flags,rc):
 def ao_receber(client,userdata,msg):
     print(f"{msg.topic} --- {msg.payload} ")
 
+cliente = mqtt.Client()
+
+
+cliente.on_connect =  ao_conectar
+
+
+cliente.connect("broker.hivemq.com",1883,60)
+cliente.subscribe("aula3a")
+cliente.loop_forever()
